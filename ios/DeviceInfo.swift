40e2cf8b298a9e3d078f1207219bc47b2bf8b8c5
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-12-02 02:17:09 +0900

    fix(ios): add nil guard for interface in getIpAddress()
    Address PR review comment #5: The force unwrap of interface?.ifa_name
    could crash if interface is nil. Use guard statement to safely unwrap
    the interface before accessing its properties.

--- ios/DeviceInfo.swift
+++ ios/DeviceInfo.swift
@@ -384,15 +384,15 @@ class DeviceInfo: HybridDeviceInfoSpec {
         while ptr != nil {
           defer { ptr = ptr?.pointee.ifa_next }
 
-          let interface = ptr?.pointee
-          let addrFamily = interface?.ifa_addr.pointee.sa_family
+          guard let interface = ptr?.pointee else { continue }
+          let addrFamily = interface.ifa_addr?.pointee.sa_family
 
           // Only process en0 (WiFi) and pdp_ip0 (cellular) interfaces
-          let name = String(cString: (interface?.ifa_name)!)
+          let name = String(cString: interface.ifa_name)
           guard name == "en0" || name == "pdp_ip0" else { continue }
 
           var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-          if getnameinfo(interface?.ifa_addr, socklen_t((interface?.ifa_addr.pointee.sa_len)!),
+          if getnameinfo(interface.ifa_addr, socklen_t((interface.ifa_addr?.pointee.sa_len)!),
                         &hostname, socklen_t(hostname.count),
                         nil, socklen_t(0), NI_NUMERICHOST) == 0 {
             let address = String(cString: hostname)
